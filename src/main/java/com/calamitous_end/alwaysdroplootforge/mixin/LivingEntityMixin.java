49b6581bcf97c3df0951f0199428a5bf892f8c33
Calamitous_End <41867693+CalamitousEnd@users.noreply.github.com>
2021-11-21 12:52:34 -0500

    Update LivingEntityMixin.java

--- src/main/java/com/calamitous_end/alwaysdroplootforge/mixin/LivingEntityMixin.java
+++ src/main/java/com/calamitous_end/alwaysdroplootforge/mixin/LivingEntityMixin.java
@@ -1,7 +1,11 @@
 package com.calamitous_end.alwaysdroplootforge.mixin;
 
 import com.calamitous_end.alwaysdroplootforge.Config;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,8 +13,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(LivingEntity.class)
-    public class LivingEntityMixin {
+public class LivingEntityMixin {
+
 
     @Shadow
     protected int lastHurtByPlayerTime;
@@ -20,30 +27,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
             method = "isAlwaysExperienceDropper",
             cancellable = true
     )
-        private void alwaysdropXp(CallbackInfoReturnable<Boolean> cir) {
-                if (Config.PASSIVE_XP_MULT.get() != 0.0) {
-                    cir.setReturnValue(true);
-                }
-        }
-        @ModifyArg(
-                at = @At(
-                        value = "INVOKE",
-                        target = "Lnet/minecraft/world/entity/ExperienceOrb;award(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;I)V"
-                ),
-                method = "dropExperience",
-                index = 2
-        )
-        private int adjustXpAmount(int originalXpAmount) {
-            if (this.lastHurtByPlayerTime <= 0) {
-                var modifier =
-                        Config.PASSIVE_XP_MULT.get();
-
-                return (int) Math.round(originalXpAmount * modifier);
-            } else {
-                return originalXpAmount;
+    private void alwaysdropxp(CallbackInfoReturnable<Boolean> cir) {
+            if (Config.PASSIVE_XP_MULT.get() != 0.0) {
+                cir.setReturnValue(true);
             }
-
-        }
     }
+
+    @ModifyArg(
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/ExperienceOrb;award(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;I)V"
+            ),
+            method = "dropExperience",
+            index = 2
+    )
+    private int adjustXpAmount(int originalXpAmount) {
+        if (this.lastHurtByPlayerTime <= 0) {
+            var modifier =
+                    Config.PASSIVE_XP_MULT.get();
+
+            return (int) Math.round(originalXpAmount * modifier);
+        } else {
+            return originalXpAmount;
+        }
+
+    }
+}
+
 
 //TODO: Implement checks for valid LivingEntities in Config.MONSTER_LIST
